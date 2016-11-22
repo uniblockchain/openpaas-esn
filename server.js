42822793b3f5fc71d859669e0aeaf1a0040d0a7a
ansuz <ansuz@transitiontech.ca>
2016-11-22 13:53:43 +0100

    restore server that I accidentally overwrote

--- server.js
+++ server.js
@@ -1,97 +1,42 @@
-/*
-    globals require console
-*/
-var Express = require('express');
-var Http = require('http');
-var Https = require('https');
-var Fs = require('fs');
-var WebSocketServer = require('ws').Server;
-var NetfluxSrv = require('./NetfluxWebsocketSrv');
+'use strict';
 
-var config = require('./config');
-var websocketPort = config.websocketPort || config.httpPort;
+var async = require('async');
+var moduleManager = require('./backend/module-manager');
+var core = require('./backend/core');
+var logger = core.logger;
+var config = core.config('default');
 
-// support multiple storage back ends
-var Storage = require(config.storage||'./storage/file');
+var modules = config.modules;
 
-var app = Express();
+moduleManager.setupServerEnvironment();
 
-var httpsOpts;
-
-var setHeaders = (function () {
-    if (typeof(config.httpHeaders) !== 'object') { return function () {}; }
-
-    var headers = JSON.parse(JSON.stringify(config.httpHeaders));
-    if (Object.keys(headers).length) {
-        return function (res) {
-            for (var header in headers) { res.setHeader(header, headers[header]); }
-        };
-    }
-    return function () {};
-}());
-
-app.use(function (req, res, next) {
-    setHeaders(res);
-    next();
-});
-
-app.use(Express.static(__dirname + config.publicPath));
-
-Fs.exists(__dirname + "/customize", function (e) {
-    if (e) { return; }
-    console.log("Cryptpad is customizable, see customize.dist/readme.md for details");
-});
-
-app.use("/customize", Express.static(__dirname + '/customize'));
-app.use("/customize", Express.static(__dirname + '/customize.dist'));
-app.use(/^\/[^\/]*$/, Express.static('customize'));
-app.use(/^\/[^\/]*$/, Express.static('customize.dist'));
-
-if (config.privKeyAndCertFiles) {
-    var privKeyAndCerts = '';
-    config.privKeyAndCertFiles.forEach(function (file) {
-        privKeyAndCerts = privKeyAndCerts + Fs.readFileSync(file);
+function fireESNState(state) {
+  return function fireESN(callback) {
+    moduleManager.manager.fire(state, modules).then(function() {
+      callback(null);
+    }, function(err) {
+      callback(err);
     });
-    var array = privKeyAndCerts.split('\n-----BEGIN ');
-    for (var i = 1; i < array.length; i++) { array[i] = '-----BEGIN ' + array[i]; }
-    var privKey;
-    for (var i = 0; i < array.length; i++) {
-        if (array[i].indexOf('PRIVATE KEY-----\n') !== -1) {
-            privKey = array[i];
-            array.splice(i, 1);
-            break;
-        }
+  };
+}
+
+function initCore(callback) {
+  core.init(function(err) {
+    if (!err) {
+      logger.info('OpenPaaS Core bootstraped, configured in %s mode', process.env.NODE_ENV);
     }
-    if (!privKey) { throw new Error("cannot find private key"); }
-    httpsOpts = {
-        cert: array.shift(),
-        key: privKey,
-        ca: array
-    };
+    callback(err);
+  });
+
 }
 
-app.get('/api/config', function(req, res){
-    var host = req.headers.host.replace(/\:[0-9]+/, '');
-    res.setHeader('Content-Type', 'text/javascript');
-    res.send('define(' + JSON.stringify({
-        websocketPath: config.websocketPath,
-        websocketURL:'ws' + ((httpsOpts) ? 's' : '') + '://' + host + ':' +
-            websocketPort + '/cryptpad_websocket',
-    }) + ');');
-});
-
-var httpServer = httpsOpts ? Https.createServer(httpsOpts, app) : Http.createServer(app);
-
-httpServer.listen(config.httpPort,config.httpAddress,function(){
-    console.log('[%s] listening on port %s', new Date().toISOString(), config.httpPort);
-});
-
-var wsConfig = { server: httpServer };
-if (websocketPort !== config.httpPort) {
-    console.log("setting up a new websocket server");
-    wsConfig = { port: websocketPort};
-}
-var wsSrv = new WebSocketServer(wsConfig);
-Storage.create(config, function (store) {
-    NetfluxSrv.run(store, wsSrv, config);
+async.series([core.templates.inject, fireESNState('lib'), initCore, fireESNState('start')], function(err) {
+  if (err) {
+    logger.error('Fatal error:', err);
+    if (err.stack) {
+      logger.error(err.stack);
+    }
+    process.exit(1);
+  }
+  logger.info('OpenPaas ESN is now started on node %s', process.version);
 });
